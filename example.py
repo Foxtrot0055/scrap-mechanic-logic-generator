2126335d8ee23c6acad89ad1acba2e1c1ca4c6c5
foxtrot <foxtrot@pop-os.localdomain>
2024-07-13 13:03:38 +0200

    add example

---
+++ example.py
@@ -0,0 +1,98 @@
+import sm_logic_lib as sm
+
+for i in range(5):
+    sm.make.logic_gate(i,0,0)
+
+#this prints:
+# {
+#    "color": "222222",
+#    "controller": {
+#       "active": false,
+#       "controllers": null,
+#       "id": 0,
+#       "joints": null,
+#       "mode": 0
+#    },
+#    "pos": {
+#       "x": 0,
+#       "y": 0,
+#       "z": 0
+#    },
+#    "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
+#    "xaxis": 0,
+#    "zaxis": 0
+# }
+# {
+#    "color": "222222",
+#    "controller": {
+#       "active": false,
+#       "controllers": null,
+#       "id": 1,
+#       "joints": null,
+#       "mode": 0
+#    },
+#    "pos": {
+#       "x": 1,
+#       "y": 0,
+#       "z": 0
+#    },
+#    "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
+#    "xaxis": 0,
+#    "zaxis": 0
+# }
+# {
+#    "color": "222222",
+#    "controller": {
+#       "active": false,
+#       "controllers": null,
+#       "id": 2,
+#       "joints": null,
+#       "mode": 0
+#    },
+#    "pos": {
+#       "x": 2,
+#       "y": 0,
+#       "z": 0
+#    },
+#    "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
+#    "xaxis": 0,
+#    "zaxis": 0
+# }
+# {
+#    "color": "222222",
+#    "controller": {
+#       "active": false,
+#       "controllers": null,
+#       "id": 3,
+#       "joints": null,
+#       "mode": 0
+#    },
+#    "pos": {
+#       "x": 3,
+#       "y": 0,
+#       "z": 0
+#    },
+#    "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
+#    "xaxis": 0,
+#    "zaxis": 0
+# }
+# {
+#    "color": "222222",
+#    "controller": {
+#       "active": false,
+#       "controllers": null,
+#       "id": 4,
+#       "joints": null,
+#       "mode": 0
+#    },
+#    "pos": {
+#       "x": 4,
+#       "y": 0,
+#       "z": 0
+#    },
+#    "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
+#    "xaxis": 0,
+#    "zaxis": 0
+# }
+
+
