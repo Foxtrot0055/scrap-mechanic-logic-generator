18a03ea28402525c30fb9e104d05d03f025ff737
foxtrot <foxtrot@pop-os.localdomain>
2024-07-13 15:12:28 +0200

    updated example.py

--- example.py
+++ example.py
@@ -5,95 +5,101 @@ for i in range(5):
 sm.make.print()
 
 #this prints:
-# {
-#    "color": "222222",
-#    "controller": {
-#       "active": false,
-#       "controllers": null,
-#       "id": 0,
-#       "joints": null,
-#       "mode": 0
-#    },
-#    "pos": {
-#       "x": 0,
-#       "y": 0,
-#       "z": 0
-#    },
-#    "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
-#    "xaxis": 0,
-#    "zaxis": 0
+#    "bodies": [
+#       {
+#          "childs": [
+#             {
+#                "color": "222222",
+#                "controller": {
+#                   "active": false,
+#                   "controllers": null,
+#                   "id": 0,
+#                   "joints": null,
+#                   "mode": 0
+#                },
+#                "pos": {
+#                   "x": 0,
+#                   "y": 0,
+#                   "z": 0
+#                },
+#                "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
+#                "xaxis": 0,
+#                "zaxis": 0
+#             },
+#             {
+#                "color": "222222",
+#                "controller": {
+#                   "active": false,
+#                   "controllers": null,
+#                   "id": 1,
+#                   "joints": null,
+#                   "mode": 0
+#                },
+#                "pos": {
+#                   "x": 1,
+#                   "y": 0,
+#                   "z": 0
+#                },
+#                "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
+#                "xaxis": 0,
+#                "zaxis": 0
+#             },
+#             {
+#                "color": "222222",
+#                "controller": {
+#                   "active": false,
+#                   "controllers": null,
+#                   "id": 2,
+#                   "joints": null,
+#                   "mode": 0
+#                },
+#                "pos": {
+#                   "x": 2,
+#                   "y": 0,
+#                   "z": 0
+#                },
+#                "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
+#                "xaxis": 0,
+#                "zaxis": 0
+#             },
+#             {
+#                "color": "222222",
+#                "controller": {
+#                   "active": false,
+#                   "controllers": null,
+#                   "id": 3,
+#                   "joints": null,
+#                   "mode": 0
+#                },
+#                "pos": {
+#                   "x": 3,
+#                   "y": 0,
+#                   "z": 0
+#                },
+#                "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
+#                "xaxis": 0,
+#                "zaxis": 0
+#             },
+#             {
+#                "color": "222222",
+#                "controller": {
+#                   "active": false,
+#                   "controllers": null,
+#                   "id": 4,
+#                   "joints": null,
+#                   "mode": 0
+#                },
+#                "pos": {
+#                   "x": 4,
+#                   "y": 0,
+#                   "z": 0
+#                },
+#                "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
+#                "xaxis": 0,
+#                "zaxis": 0
+#             }
+#          ]
+#       }
+#    ],
+#    "version": 4
 # }
-# {
-#    "color": "222222",
-#    "controller": {
-#       "active": false,
-#       "controllers": null,
-#       "id": 1,
-#       "joints": null,
-#       "mode": 0
-#    },
-#    "pos": {
-#       "x": 1,
-#       "y": 0,
-#       "z": 0
-#    },
-#    "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
-#    "xaxis": 0,
-#    "zaxis": 0
-# }
-# {
-#    "color": "222222",
-#    "controller": {
-#       "active": false,
-#       "controllers": null,
-#       "id": 2,
-#       "joints": null,
-#       "mode": 0
-#    },
-#    "pos": {
-#       "x": 2,
-#       "y": 0,
-#       "z": 0
-#    },
-#    "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
-#    "xaxis": 0,
-#    "zaxis": 0
-# }
-# {
-#    "color": "222222",
-#    "controller": {
-#       "active": false,
-#       "controllers": null,
-#       "id": 3,
-#       "joints": null,
-#       "mode": 0
-#    },
-#    "pos": {
-#       "x": 3,
-#       "y": 0,
-#       "z": 0
-#    },
-#    "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
-#    "xaxis": 0,
-#    "zaxis": 0
-# }
-# {
-#    "color": "222222",
-#    "controller": {
-#       "active": false,
-#       "controllers": null,
-#       "id": 4,
-#       "joints": null,
-#       "mode": 0
-#    },
-#    "pos": {
-#       "x": 4,
-#       "y": 0,
-#       "z": 0
-#    },
-#    "shapeId": "9f0f56e8-2c31-4d83-996c-d00a9b296c3f",
-#    "xaxis": 0,
-#    "zaxis": 0
-# }
-
-
