3569f25027831077cb894d62f63bd96925b78637
foxtrot <foxtrot@pop-os.localdomain>
2024-07-14 17:23:03 +0200

    changed input to output added rc adder

--- rc_adder.py
+++ rc_adder.py
@@ -1,22 +1,23 @@
 import sm_logic_lib as sm
 
-def full_adder(pos_x, pos_y, pos_z, cin_id):
-    a = sm.make.logic_gate(pos_x, pos_y, pos_z)
-    b = sm.make.logic_gate(pos_x, pos_y+1, pos_z)
-    cin = sm.make.logic_gate(pos_x, pos_y+2, pos_z, [cin_id])
+def full_adder(pos_x, pos_y, pos_z, carry_out):
+    cout = sm.make.logic_gate(pos_x, pos_y, pos_z, [carry_out], [0,0], 1)
+    s = sm.make.logic_gate(pos_x, pos_y+1, pos_z, None, [0,0], 2)
+    
+    ab = sm.make.logic_gate(pos_x+1,pos_y,pos_z,[cout])
+    ac = sm.make.logic_gate(pos_x+1,pos_y+1,pos_z, [cout])
+    bc = sm.make.logic_gate(pos_x+1,pos_y+2,pos_z, [cout])
 
-    d = sm.make.logic_gate(pos_x+2, pos_y+2, pos_z,[a,b,cin],"222222",0,0,2)
-    ab = sm.make.logic_gate(pos_x+1, pos_y+1, pos_z, [a, b]) 
-    ac = sm.make.logic_gate(pos_x+1, pos_y, pos_z, [a, cin]) 
-    bc = sm.make.logic_gate(pos_x+1,pos_y, pos_z, [b, cin])
+    a = sm.make.logic_gate(pos_x+2,pos_y,pos_z,[ab, ac])
+    b = sm.make.logic_gate(pos_x+2,pos_y+1,pos_z,[ab, bc])
+    cin = sm.make.logic_gate(pos_x+2,pos_y+2,pos_z,[ac, bc])
 
-    cout = sm.make.logic_gate(pos_x+2, pos_y+1, pos_z, [ab, ac, bc], "222222", 0, 0, 1) 
-    return cout
+    return cin
 
 def rc_adder(pos_x, pos_y, pos_z, bitwidth):
-    cout = sm.make.logic_gate(pos_x,pos_y+3,pos_z)
+    cin = None
     for bit in range(bitwidth):
-        cout = full_adder(pos_x, pos_y+-3*bit, pos_z, cout)
+        cin = full_adder(pos_x, pos_y+bit*3, pos_z, cin)
 
-rc_adder(0, 0, 0, 8)
+rc_adder(0,0,0,8)
 sm.make.print()
--- sm_logic_lib.py
+++ sm_logic_lib.py
@@ -3,14 +3,14 @@ import json
 class make():
     blueprint = {'bodies': [{'childs': []}],'version': 4}
     id_count = 0
-    def logic_gate(pos_x, pos_y, pos_z, input=None, color="222222", rot_x=0, rot_z=0, type=0):
-        input_list = []
-        if input != None: 
-            for id in input:
-                input_list.append({'id': id})
-        make.blueprint['bodies'][0]['childs'].append({'color': color, 'controller': {'active': False, 'controllers': input_list, 'id': make.id_count, 'joints': None, 'mode': type}, 'pos': {'x': pos_x, 'y': pos_y, 'z': pos_z}, 'shapeId': "9f0f56e8-2c31-4d83-996c-d00a9b296c3f", 'xaxis': rot_x, 'zaxis': rot_z})
-        return make.id_count
+    def logic_gate(pos_x, pos_y, pos_z, output=None, rot=[0,0], type=0, color="222222"):
+        output_list = []
+        if output != None: 
+            for id in output:
+                output_list.append({'id': id})
+        else: output_list=None
+        make.blueprint['bodies'][0]['childs'].append({'color': color, 'controller': {'active': False, 'controllers': output_list, 'id': make.id_count, 'joints': None, 'mode': type}, 'pos': {'x': pos_x, 'y': pos_y, 'z': pos_z}, 'shapeId': "9f0f56e8-2c31-4d83-996c-d00a9b296c3f", 'xaxis': rot[0], 'zaxis': rot[1]})
         make.id_count += 1
-        print(id_count)
+        return make.id_count-1
     def print():
         print(json.dumps(make.blueprint, indent=3))
